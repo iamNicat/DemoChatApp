fd8e40e543bdc42e638eed5d087c9168161e4f11
Your Name <you@example.com>
2020-05-25 19:44:01 +0400

    I connected our projet to Firebase

--- SimpleChat/app/src/main/java/com/example/simplechat/MainActivity.kt
+++ SimpleChat/app/src/main/java/com/example/simplechat/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.simplechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+    }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun updateUI(currentUser: FirebaseUser?) {
+
     }
 }
---
+++ SimpleChat/app/src/main/java/com/example/simplechat/User.kt
@@ -0,0 +1,4 @@
+package com.example.simplechat
+
+class User {
+}
